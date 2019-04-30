b5fe6f09242f6df9b72d80c545ff8c86b91186a8
Lucas Queiroz <lucasmedeirosq@gmail.com>
2019-04-30 04:32:57 -0300

    heroku app

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const http = require('http')
 
-const port = 3000
-const ip = '192.168.2.13'
+const port = Process.env.PORT || 3000
+const ip = 'localhost'
 
 const app = express();
 const server = http.createServer(app);
